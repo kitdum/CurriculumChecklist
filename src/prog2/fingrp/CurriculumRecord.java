70bff92d901b84f15e2985f076fb74cf10710a93
illogicbit <henri.l.agustin@gmail.com>
2024-05-01 18:00:16 +0800

    Removed test constructor

--- src/prog2/fingrp/CurriculumRecord.java
+++ src/prog2/fingrp/CurriculumRecord.java
@@ -9,33 +9,6 @@ public class CurriculumRecord {
     private ArrayList<Course> personalRecord;
     private ArrayList<Course> compiledRecord;
 
-    public CurriculumRecord(){
-        //Load test data... this isn't permanent.
-        this.templateRecord = new ArrayList<Course>();
-        templateRecord.add(new Course("CS111","Intro to Computing",1,1,2));
-        templateRecord.add(new Course("CS122","Test1",1,2,1));
-        templateRecord.add(new Course("CS123","T2t",1,2,3));
-        templateRecord.add(new Course("CS212","tas",2,1,5));
-
-        this.personalRecord = new ArrayList<Course>();
-        Course data = new Course("CS122","God died for our sins",-1,-1,-1);
-        data.setGrade(50);
-        personalRecord.add(data);
-        data = new Course("CS111", "AAAAAAAAA",-1,-1,-1);
-        data.setGrade(95);
-        personalRecord.add(data);
-        data = new Course("CS212", "AAAAsdas",-1,-1,-1);
-        data.setGrade(80);
-        personalRecord.add(data);
-        data = new Course("CS212", "Baba",-1,-1,-1);
-        data.setGrade(75);
-        personalRecord.add(data);
-
-        compiledRecord = this.templateRecord;
-        recompileRecord();
-
-    }
-
     public CurriculumRecord(InputStream templateRecord, InputStream personalRecord) throws IOException, ClassNotFoundException{
         this.templateRecord = (ArrayList<Course>) new ObjectInputStream(templateRecord).readObject();
         this.personalRecord = (ArrayList<Course>) new ObjectInputStream(personalRecord).readObject();
